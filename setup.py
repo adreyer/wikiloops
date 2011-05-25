e70ddfc26c7b4155a9a0dbc7d56cc855bd63106b
Alex Dreyer <alex.dreyer@cmgdigital.com>
2011-05-25 18:43:21 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='wikiloops',
+      version='0.1',
+      description='wikipedia graphs',
+      author='Alex Dreyer',
+      author_email='amd@mdc2.org',
+      modules=['wikiloops'],
+     )
